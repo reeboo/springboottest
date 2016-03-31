ecf235a1e165c3d18f2f63744c322d167b7a4f52
reeboo <ruibo.ma@qunar.com>
2016-03-31 14:43:12 +0800

    springboottest

---
+++ src/main/java/com/spring/boot/test/Main.java
@@ -0,0 +1,41 @@
+package com.spring.boot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 实现描述: Main
+ *
+ * @version v1.0.0
+ * @author: reeboo
+ * @since: 2016-03-31 11:50
+ */
+@SpringBootApplication
+@RestController
+public class Main extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(8081);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Main.class);
+    }
+
+    @RequestMapping("/")
+    String index() {
+        return "just a test";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
